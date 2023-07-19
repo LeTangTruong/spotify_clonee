f4daf8428c21e9d35709592ca9a890a756d2971f
TruongBob <letangtruong2901@gmail.com>
2023-07-19 11:05:21 +0700

    LTT

---
+++ components/Button.tsx
@@ -0,0 +1,26 @@
+import React, {forwardRef} from "react";
+import {twMerge} from "tailwind-merge";
+
+interface ButtonProps
+    extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+}
+
+const Button = forwardRef<HTMLButtonElement,ButtonProps>(({
+className,
+    children,
+    disabled,
+    type="button",
+    ...props
+},ref)=>{
+    return(
+        <button
+        type={type}
+        className={twMerge(`
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+        `)}>
+
+        </button>
+    )
+})
+Button.displayName = "Button";
+export default Button;
